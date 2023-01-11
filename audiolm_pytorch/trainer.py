ad1f733a16217ffe76ae34987839e71450316696
djqualia <jbickmore@gmail.com>
2023-01-10 23:42:15 -0700

    Fix typo

--- audiolm_pytorch/trainer.py
+++ audiolm_pytorch/trainer.py
@@ -521,7 +521,7 @@ class SemanticTransformerTrainer(nn.Module):
         self.results_folder.mkdir(parents = True, exist_ok = True)
         
         hps = {"num_train_steps": num_train_steps, "data_max_length": data_max_length, "learning_rate": lr}
-        self.accelerator.init_trackers("semenatic", config=hps)
+        self.accelerator.init_trackers("semantic", config=hps)
 
     def save(self, path):
         pkg = dict(
