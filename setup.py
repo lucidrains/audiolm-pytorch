ce9c1a8c7ad5aec6724e1fd78886ccced368c2bb
Phil Wang <lucidrains@gmail.com>
2023-02-03 07:25:26 -0800

    0.10.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'audiolm-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.10.1',
+  version = '0.10.2',
   license='MIT',
   description = 'AudioLM - Language Modeling Approach to Audio Generation from Google Research - Pytorch',
   author = 'Phil Wang',
