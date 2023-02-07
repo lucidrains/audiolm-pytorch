e3a4e89748b4a687ad55a4b7bec0f5727a96ae10
Phil Wang <lucidrains@gmail.com>
2023-02-07 14:03:34 -0800

    should use absolute value of complex value for final loss

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'audiolm-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.11.2',
+  version = '0.11.3',
   license='MIT',
   description = 'AudioLM - Language Modeling Approach to Audio Generation from Google Research - Pytorch',
   author = 'Phil Wang',
