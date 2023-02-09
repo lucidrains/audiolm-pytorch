d700bc3b0cbca171734c0f18f86b78bdc1ed466a
Phil Wang <lucidrains@gmail.com>
2023-02-09 09:36:05 -0800

    need latest version of torch for complex stft discriminator distributed training

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'audiolm-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.11.16',
+  version = '0.11.17',
   license='MIT',
   description = 'AudioLM - Language Modeling Approach to Audio Generation from Google Research - Pytorch',
   author = 'Phil Wang',
@@ -28,7 +28,7 @@ setup(
     'Mega-pytorch',
     'scikit-learn',
     'sentencepiece',
-    'torch>=1.6',
+    'torch>=1.12',
     'torchaudio',
     'transformers',
     'tqdm',
