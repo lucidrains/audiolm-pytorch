b04d207d894f8563ce20702cec8e09af56d2b82f
Hayk Martiros <hayk.mart@gmail.com>
2023-03-25 23:57:16 -0700

    Option to pass in Accelerator to SoundStreamTrainer
    This is needed for multi-GPU use cases where certain actions need
    to be performed on the main process before the trainer is constructed.
    
    Also minor tweak to accept more audio types in the data loader.

--- audiolm_pytorch/data.py
+++ audiolm_pytorch/data.py
@@ -36,7 +36,7 @@ class SoundDataset(Dataset):
     def __init__(
         self,
         folder,
-        exts = ['flac', 'wav'],
+        exts = ['flac', 'wav', 'mp3', 'webm'],
         max_length: OptionalIntOrTupleInt = None,
         target_sample_hz: OptionalIntOrTupleInt = None,
         seq_len_multiple_of: OptionalIntOrTupleInt = None
