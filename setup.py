3ecad1458ab44a3a95e8783316e3b6ec7a7657e5
lucidrains <lucidrains@gmail.com>
2024-11-06 06:04:10 -0800

    fix an issue with quantize dropout for residual finite scalar quantization in distributed environment

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     'torchaudio',
     'transformers',
     'tqdm',
-    'vector-quantize-pytorch>=1.18.1'
+    'vector-quantize-pytorch>=1.18.6'
   ],
   classifiers=[
     'Development Status :: 4 - Beta',
