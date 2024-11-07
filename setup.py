ade4fd6cf282f115b2c91c7969768ce2289b8171
lucidrains <lucidrains@gmail.com>
2024-11-07 04:59:01 -0800

    update vq again

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     'torchaudio',
     'transformers',
     'tqdm',
-    'vector-quantize-pytorch>=1.18.7'
+    'vector-quantize-pytorch>=1.18.8'
   ],
   classifiers=[
     'Development Status :: 4 - Beta',
